f37340f5dd3b1932d6e7c40f75df1155bbcb5750
Peng-Yu Chen <walkccray@gmail.com>
2022-01-24 12:56:54 -0500

    Parallel it

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 
+import multiprocessing as mp
+
 from leetcode_api import LeetCodeAPI
 
 if __name__ == "__main__":
   leetcode_api = LeetCodeAPI(sys.argv)
-  leetcode_api.write_problems()
+  pool = mp.Pool(mp.cpu_count())
+  pool.apply(leetcode_api.write_problems)
+  pool.close()
   leetcode_api.write_mkdocs()
   leetcode_api.write_readme()
