221054840018c2f3a0abeb46c08fb01cd16721ba
Peng-Yu Chen <walkccray@gmail.com>
2022-01-24 12:10:34 -0500

    Rename leetcode_mkdocs.py to main.py

--- mock.sh
+++ mock.sh
@@ -5,7 +5,7 @@ cd LeetCode
 git clone -b mkdocs --single-branch https://github.com/walkccc/LeetCode.git mkdocs
 git clone -b scripts --single-branch https://github.com/walkccc/LeetCode.git scripts
 
-python3 scripts/leetcode_mkdocs.py --mock
+python3 scripts/main.py --mock
 
 cp README.md mkdocs/docs/preface.md
 cp NAMING.md mkdocs/docs/naming.md
